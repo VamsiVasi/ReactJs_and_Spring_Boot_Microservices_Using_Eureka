2dc79b5efed916b1f024c4da3fdcb0e8c6f3cf1b
VamsiVasi <vasikrish999@gmail.com>
2022-09-02 21:24:02 +0530

    Api Gateway for the Microservices Using Eureka Client

---
+++ Eureka_Api_Gateway/src/main/java/com/stark/api_gateway/EurekaApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.stark.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApiGatewayApplication.class, args);
+	}
+
+}
